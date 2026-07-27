c43ef3f7ed9de8559e0bd742a155bc29f3687de5
IDoTweaks <idotweaks@gmail.com>
2026-07-27 19:26:54 +0300

    applyForces to player

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -6,15 +6,18 @@ var forcesX : Array
 var forcesY : Array
 var forcesZ : Array
 var forceTime : Array
+var forceDecay : Array
 
 @onready var player_cam: Camera3D = $playerCam
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
-func _applyForceFromPoint(point, force, time):
+
+func _applyForceFromPoint(point, force, time,decay = 0):
 	#this math is brought to you by: gemini but i coded it:D
 	forceTime.append(time)
+	forceDecay.append(decay)
 	var unWeightedX = abs(global_position.x - point.x)
 	var unWeightedY = abs(global_position.y - point.y)
 	var unWeightedZ = abs(global_position.z - point.z)
@@ -29,9 +32,10 @@ func _applyForceFromPoint(point, force, time):
 	forcesZ.append(sign(global_position.z - point.z) * weightedZ * force)
 	forcesY.append(sign(global_position.y - point.y) * weightedY * force)
 
-func _applyForceToPoint(point, force, time):
+func _applyForceToPoint(point, force, time,decay = 0):
 	#this math is brought to you by: gemini but i coded it:D
 	forceTime.append(time)
+	forceDecay.append(decay)
 	var unWeightedX = abs(point.x-global_position.x)
 	var unWeightedY = abs(point.y-global_position.y )
 	var unWeightedZ = abs(point.z-global_position.z)
@@ -51,9 +55,37 @@ func _unhandled_input(event: InputEvent) -> void:
 		rotate_y(-event.relative.x * MOUSE_SENSITIVITY)
 		player_cam.rotate_x(-event.relative.y * MOUSE_SENSITIVITY)
 		player_cam.rotation.x = clamp(player_cam.rotation.x, deg_to_rad(-89.0), deg_to_rad(89.0))
-
+	
 	if event.is_action_pressed("ui_cancel"):
 		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 		else:
 			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _applyForceDecay(i,delta):
+	forcesX[i] = move_toward(forcesX[i], 0, forceDecay[i] * delta)
+	forcesY[i] = move_toward(forcesY[i], 0, forceDecay[i] * delta)
+	forcesZ[i] = move_toward(forcesZ[i], 0, forceDecay[i] * delta)
+	
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	#apply forces
+	for i in range(forceTime.size()):
+		if forceTime[i] > 0:
+			forceTime[i] -= delta
+			velocity.x += forcesX[i] * delta
+			velocity.z += forcesZ[i] * delta
+			velocity.y += forcesY[i] * delta
+			_applyForceDecay(i,delta)
+		else:
+			forceTime.remove_at(i)
+			forcesX.remove_at(i)
+			forcesZ.remove_at(i)
+			forcesY.remove_at(i)
+			forceDecay.remove_at(i)
+	
+	
+	
+	move_and_slide()
